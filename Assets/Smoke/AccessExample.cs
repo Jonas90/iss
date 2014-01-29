36c08997aac9d4a88d9be015a7a60780241bc2af
Daniel Chumak <s0535417@htw-berlin.de>
2014-01-29 16:56:28 +0100

    ben's smoke

---
+++ Assets/Smoke/AccessExample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccessExample : MonoBehaviour {
+
+	GameObject smoke;
+	SmokeController smokeController;
+	
+	// Use this for initialization
+	void Start () {
+		
+		// Create a reference to smoke GameObject
+		smoke = GameObject.Find("SmokeParticleSystem");
+		
+		// Create a reference to smokeController script
+		smokeController = smoke.GetComponent<SmokeController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			
+			// Set smokeController.play to true activates smoke emission, false disable smoke emission
+			smokeController.play = !smokeController.play;
+		}
+	}
+}
---
+++ Assets/Smoke/SmokeController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmokeController : MonoBehaviour {
+	
+	public bool play;
+	public float spreadFactor;
+	private bool playing;
+	private float lifeTimeDefault;
+	
+	void Awake () {
+		play = true;
+		playing = false;
+		lifeTimeDefault = 1;
+		spreadFactor = 1.0f;
+		particleSystem.Stop();
+		particleSystem.startSize = 2.0f;
+		particleSystem.startSpeed = 0.2f;
+		particleSystem.emissionRate = 50;
+		particleSystem.startLifetime = lifeTimeDefault;
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+		// Starts smoke emission
+		if (play && !playing) {
+			particleSystem.startLifetime = lifeTimeDefault;
+			particleSystem.Play();
+			playing = true;
+			Debug.Log("Rauchentwicklung gestartet");
+		}
+		
+		// Stops smoke emission
+		if (!play && playing) {
+			particleSystem.Stop();
+			playing = false;
+			Debug.Log("Rauchentwicklung gestoppt");
+		}
+		
+		// Increases startLifetime depending on duration of smoke emission
+		if (particleSystem.isPlaying) {
+			particleSystem.startLifetime += (float) Time.deltaTime * spreadFactor;
+		}
+			
+		
+		// Veraendern von groesse, geschwindigkeit und lebensdauer
+		if (Input.GetKeyDown(KeyCode.F1))
+			particleSystem.startSize--;
+		if (Input.GetKeyDown(KeyCode.F2))
+			particleSystem.startSize++;
+		if (Input.GetKeyDown(KeyCode.F3))
+			particleSystem.startSpeed--;
+		if (Input.GetKeyDown(KeyCode.F4))
+			particleSystem.startSpeed++;	
+		if (Input.GetKeyDown(KeyCode.F5))
+			particleSystem.startLifetime--;
+		if (Input.GetKeyDown(KeyCode.F6))
+			particleSystem.startLifetime++;
+	}
+}
